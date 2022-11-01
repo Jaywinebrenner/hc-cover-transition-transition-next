e5b916c257ca32e4603389212490dfcf07f9a06e
Jay WInebrenner <jaywinebrenner@gmail.com>
2022-11-01 09:15:19 -0700

    first commit

---
+++ utils/fakedata.js
@@ -0,0 +1,54 @@
+
+
+export const fakeData = [
+    {
+        itemTitle: 'Jon McPerson',
+        itemContent: 'Et Magna etiam tempor orci eu lobortis Quam pellentesque nec nam aliquam sem et.  elementum. Aenean et tortor at risus viverra adipiscing at in. Maecenas accumsan lacus vel facilisis volutpat. Morbi non arcu risus quis. ',
+        previewTitle: 'McPerson',
+        previewName: 'Jon McPerson',
+        questionOneAnswer: 'Mauve',
+        questionTwoAnswer: 'Turkey Sandwich',
+        url: 'img/0.jpg',
+        urlBig: 'img/0_big.jpg'
+    },
+    {
+        itemTitle: 'Samamtha Smith',
+        itemContent: 'Quam pellentesque nec nam aliquam sem et. Magna etiam tempor orci eu lobortis elementum. Aenean et tortor at risus viverra adipiscing at in. Maecenas accumsan lacus vel facilisis volutpat. Morbi non arcu risus quis. ',
+        previewTitle: 'Smith',
+        previewName: 'Samamtha Smith',
+        questionOneAnswer: 'Blue or Green',
+        questionTwoAnswer: 'Olives',
+        url: 'img/2.jpg',
+        urlBig: 'img/2_big.jpg'
+    },
+    {
+        itemTitle: 'Greg Daniels',
+        itemContent: 'Magna etiam tempor orci eu lobortis elementum. Aenean et tortor at risus viverra adipiscing at in. ',
+        previewTitle: 'Daniels',
+        previewName: 'Greg Daniels',
+        questionOneAnswer: 'Purlle',
+        questionTwoAnswer: 'Chicken Tikka Masala',
+        url: 'img/3.jpg',
+        urlBig: 'img/3_big.jpg'
+    },
+    {
+        itemTitle: 'Phillip McDillip',
+        itemContent: 'Quam pellentesque nec nam aliquam sem et. Tempor orci eu lobortis elementum. Aenean et tortor at risus viverra adipiscing at in. Maecenas accumsan lacus vel facilisis volutpat. Morbi non arcu risus quis. ',
+        previewTitle: 'McDillip',
+        previewName: 'Phillip McDillip',
+        questionOneAnswer: 'Dark green',
+        questionTwoAnswer: 'Mexican',
+        url: 'img/4.jpg',
+        urlBig: 'img/4_big.jpg'
+    },
+    {
+        itemTitle: 'Jennifer McPerson',
+        itemContent: ' Morbi non arcu risus quis. ',
+        previewTitle: 'McPerson',
+        previewName: 'Jennifer McPerson',
+        questionOneAnswer: 'Black',
+        questionTwoAnswer: 'Hot Dogs',
+        url: 'img/5.jpg',
+        urlBig: 'img/5_big.jpg'
+    },
+]
